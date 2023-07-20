2792ada0a82d722cba94c2717bd047660be13339
Raihan Firmansyah <raihan.firm@gmail.com>
2023-07-20 17:31:44 +0700

    feat: dark mode

--- src/app/pages/HomePage/section/hero.tsx
+++ src/app/pages/HomePage/section/hero.tsx
@@ -3,13 +3,13 @@ import * as React from 'react';
 export function Hero() {
   return (
     <>
-      <section id="home" className="pt-24">
+      <section id="home" className="pt-24 dark:bg-dark">
         <div className="container">
           <div className="flex flex-wrap">
             <div className="w-full self-center px-4 md:pl-10 lg:w-1/2">
               <h1 className="text-base font-semibold text-primary md:text-xl">
                 Hello everyone 👋, My name is{' '}
-                <span className="block font-bold text-dark text-4xl mt-1 lg:text-5xl">
+                <span className="block font-bold text-dark text-4xl mt-1 lg:text-5xl dark:text-white">
                   Raihan Firmansyah
                 </span>
               </h1>
