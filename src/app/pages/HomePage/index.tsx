280e0208ecd8970d1c8890de50fa0d08fe104626
Raihan Firmansyah <raihan.firm@gmail.com>
2023-07-22 00:02:52 +0700

    feat: website traffic counter

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -6,10 +6,30 @@ import { Certification } from './section/certification';
 import { Contact } from './section/contact';
 import { Footer } from './../../components/footer';
 import { Helmet } from 'react-helmet-async';
+import axios from 'axios';
+
+const scriptURL = String(process.env.REACT_APP_SCRIPT_URL);
 
 export function HomePage() {
+  let form: HTMLFormElement;
+
+  React.useEffect(() => {
+    if (sessionStorage.getItem('counter') !== 'true') {
+      sessionStorage.setItem('counter', 'true');
+      let formData = new FormData(form);
+      axios.post(scriptURL, formData).catch(error => {
+        sessionStorage.setItem('counter', 'false');
+        console.error(error.message);
+      });
+    }
+  });
+
   return (
     <>
+      <form className="hidden" ref={el => (form = el as HTMLFormElement)}>
+        <input type="hidden" value={process.env.NODE_ENV} name="environment" />
+        <input type="hidden" value="counter" name="type" />
+      </form>
       <Helmet>
         <title>Home Page</title>
         <meta name="description" content="A Boilerplate application homepage" />
