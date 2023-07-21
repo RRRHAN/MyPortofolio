b529c240f182a916fd289bbf5e64b87e2787c93c
Raihan Firmansyah <63929210+RRRHAN@users.noreply.github.com>
2023-07-21 15:47:05 +0700

    [FEAT] animate the component (#26)

--- src/app/pages/HomePage/section/contact.tsx
+++ src/app/pages/HomePage/section/contact.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import axios from 'axios';
 import Modal from 'react-modal';
 import styled from 'styled-components/macro';
+import { Animate } from 'app/components/animate';
 
 const scriptURL = String(process.env.REACT_APP_CONTACT_FORM_SCRIPT_URL);
 
@@ -52,70 +53,80 @@ export function Contact() {
       <section id="contact" className="pt-20 pb-32 dark:bg-slate-800">
         <div className="container">
           <div className="w-full px-4">
-            <div className="max-w-xl mx-auto text-center mb-16">
-              <h4 className="font-semibold text-lg text-primary mb-2">
-                Contact
-              </h4>
-              <h2 className="font-bold text-dark text-3xl mb-4 sm:text-4xl lg:text-5xl dark:text-white">
-                Contact Form
-              </h2>
-              <p className="font-medium text-base text-secondary md:text-lg">
-                Get in touch for collaboration opportunities and exciting tech
-                discussions. Let's innovate together!
-              </p>
-            </div>
+            <Animate from="top">
+              <div className="max-w-xl mx-auto text-center mb-16">
+                <h4 className="font-semibold text-lg text-primary mb-2">
+                  Contact
+                </h4>
+                <h2 className="font-bold text-dark text-3xl mb-4 sm:text-4xl lg:text-5xl dark:text-white">
+                  Contact Form
+                </h2>
+                <p className="font-medium text-base text-secondary md:text-lg">
+                  Get in touch for collaboration opportunities and exciting tech
+                  discussions. Let's innovate together!
+                </p>
+              </div>
+            </Animate>
           </div>
 
           <form onSubmit={e => submitForm(e)} ref={el => (form = el)}>
             <div className="w-full lg:w-2/3 lg:mx-auto">
               <div className="w-full px-4 mb-8">
-                <label
-                  htmlFor="name"
-                  className="text-base text-primary font-bold"
-                >
-                  Name
-                </label>
-                <input
-                  required
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"
-                />
+                <Animate from="left">
+                  <label
+                    htmlFor="name"
+                    className="text-base text-primary font-bold"
+                  >
+                    Name
+                  </label>
+                  <input
+                    required
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"
+                  />
+                </Animate>
               </div>
               <div className="w-full px-4 mb-8">
-                <label
-                  htmlFor="email"
-                  className="text-base text-primary font-bold"
-                >
-                  Email
-                </label>
-                <input
-                  required
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"
-                />
+                <Animate from="right">
+                  <label
+                    htmlFor="email"
+                    className="text-base text-primary font-bold"
+                  >
+                    Email
+                  </label>
+                  <input
+                    required
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary"
+                  />
+                </Animate>
               </div>
               <div className="w-full px-4 mb-8">
-                <label
-                  htmlFor="message"
-                  className="text-base text-primary font-bold"
-                >
-                  Message
-                </label>
-                <textarea
-                  required
-                  id="message"
-                  name="message"
-                  className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary h-32"
-                ></textarea>
+                <Animate from="left">
+                  <label
+                    htmlFor="message"
+                    className="text-base text-primary font-bold"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    required
+                    id="message"
+                    name="message"
+                    className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary h-32"
+                  ></textarea>
+                </Animate>
               </div>
               <div className="w-full px-4">
-                <button className="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500">
-                  Send
-                </button>
+                <Animate from="right">
+                  <button className="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500">
+                    Send
+                  </button>
+                </Animate>
               </div>
             </div>
           </form>
--- src/app/pages/HomePage/section/hero.tsx
+++ src/app/pages/HomePage/section/hero.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { motion } from 'framer-motion';
+import { Animate } from 'app/components/animate';
 
 export function Hero() {
   return (
@@ -7,34 +9,41 @@ export function Hero() {
         <div className="container">
           <div className="flex flex-wrap">
             <div className="w-full self-center px-4 md:pl-10 lg:w-1/2">
-              <h1 className="text-base font-semibold text-primary md:text-xl">
-                Hello everyone 👋, My name is{' '}
-                <span className="block font-bold text-dark text-4xl mt-1 lg:text-5xl dark:text-white">
-                  Raihan Firmansyah
-                </span>
-              </h1>
-              <h2 className="font-medium text-secondary text-lg mb-5 mt-1 lg:text-2xl">
-                BackEnd Engineer | Machine Learning Enthusiast
-              </h2>
-              <p className="font-medium text-secondary mb-10 leading-relaxed">
-                God never promise life would be easy but he promise after
-                hardship will be ease
-              </p>
-              <a
-                href="mailto:raihan.firmansy@gmail.com"
-                className="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out"
-              >
-                Contact Me
-              </a>
+              <Animate from="left">
+                <h1 className="text-base font-semibold text-primary md:text-xl">
+                  Hello everyone 👋, My name is{' '}
+                  <span className="block font-bold text-dark text-4xl mt-1 lg:text-5xl dark:text-white">
+                    Raihan Firmansyah
+                  </span>
+                </h1>
+                <h2 className="font-medium text-secondary text-lg mb-5 mt-1 lg:text-2xl">
+                  BackEnd Engineer | Machine Learning Enthusiast
+                </h2>
+                <p className="font-medium text-secondary mb-10 leading-relaxed">
+                  God never promise life would be easy but he promise after
+                  hardship will be ease
+                </p>
+                <a href="mailto:raihan.firmansy@gmail.com">
+                  <motion.button
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    className="button text-base font-semibold text-white bg-primary py-3 px-8 rounded-full hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out"
+                  >
+                    Contact Me
+                  </motion.button>
+                </a>
+              </Animate>
             </div>
             <div className="w-full self-end px-4 lg:w-1/2">
-              <div className="mt-10">
-                <img
-                  src="/raihan.jpg"
-                  alt="Raihan Firmansyah"
-                  className="max-w-full mx-auto rounded-2xl w-56 border-8 md:w-72 lg:w-80 border-primary rotate-2  hover:-rotate-3 transition duration-300 ease-in-out"
-                />
-              </div>
+              <Animate from="right">
+                <div className="mt-10">
+                  <img
+                    src="/raihan.jpg"
+                    alt="Raihan Firmansyah"
+                    className="max-w-full mx-auto rounded-2xl w-56 border-8 md:w-72 lg:w-80 border-primary rotate-2  hover:-rotate-3 transition duration-300 ease-in-out"
+                  />
+                </div>
+              </Animate>
             </div>
           </div>
         </div>
